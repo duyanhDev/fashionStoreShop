67c0e1f4d43f8e00dd5c28a1b33aa0f57ecbf1f6
DuyanhDev <166149554+duyanhDev@users.noreply.github.com>
2025-02-20 23:18:38 +0700

    Deloy be 24

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       // "http://localhost:5173",
-      "https://fashion-store-shop-git-frontend-duyanhdevs-projects.vercel.app/",
+      "https://fashion-store-shop-uinw.vercel.app/",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
@@ -28,7 +28,7 @@ app.use(
   cors({
     origin: [
       // "http://localhost:5173",
-      "https://fashion-store-shop-git-frontend-duyanhdevs-projects.vercel.app/",
+      "https://fashion-store-shop-uinw.vercel.app/",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
