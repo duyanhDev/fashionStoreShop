d4c93bc67fe0a985dbb64e98e1bdd94b73e9d26b
DuyanhDev <166149554+duyanhDev@users.noreply.github.com>
2025-06-19 23:27:59 +0700

    BE 19-6

--- src/server.js
+++ src/server.js
@@ -68,7 +68,6 @@ const config = {
 app.post("/zalopay-callback", async (req, res) => {
   try {
     const callback_data = req.body;
-    console.log("Received callback data:", callback_data);
 
     // Verify callback data
     const data =
